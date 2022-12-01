e4966f4fe778ea6f80087f74334a89966261f763
Sumalathakurupati <97958219+Sumalathakurupati@users.noreply.github.com>
2022-12-01 11:49:24 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+import "variables"
+//declare a variable
+
+func main(){
+var name string
+//Declaration with initialization
+var Name1 string = "John"
+
+//Declaration with type inference
+
+var age = 32
+
+//Declaration of multiple variables
+
+var name2, age1 = "Martin",43
+
+//shorthand declaration
+
+name3 := "JohnMartin"
+
+StudentName
+
+//static type
+//dynamic type
+
+//scope of variables
+Data types
+const pi = 3.124
+const website = "http://localhost:8080/login"
+
+}
